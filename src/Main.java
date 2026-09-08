4c74a5ae12915527d4046755bb4d012093e5b78d
gomezsan2233 <santinogomez.j@hotmail.com>
2026-09-07 23:59:16 -0300

    Se agrego el switch
    Agregue la estructura del switch

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,42 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
 
-    System.out.println("67");
+    Scanner scanner = new Scanner(System.in);
+    int i=0;
+    boolean incorrect;
+    String seguir = "si";
+//while desea continuar
+    while (seguir.equalsIgnoreCase("si")) {
+        //while ERROR
+        do {
+            incorrect = false;
+
+            int opcion = 0;
+            System.out.println("MENU\n");
+
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+            switch (opcion) {
+                case 1:
+
+                    break;
+
+                default:
+                    System.out.println("ERROR defaulting to main MENU\n");
+                    incorrect = true;
+                    break;
+            }
+        } while (incorrect);
+        System.out.println("Desea continuar en el MENU? \nSi-No\n");
+        seguir = scanner.nextLine();
+        scanner.nextLine();
+    }
 
 
 
 
 }
+
+
+
+
