d8ba840453e8e3026b65a08bae8452a42ef4a2e4
gomezsan2233 <santinogomez.j@hotmail.com>
2026-09-09 12:22:42 -0300

    Se agregaron lineas de codigo
    commit breve para hacer la modificacion mas facil

--- src/ClubDeportivo/model/Deporte.java
+++ src/ClubDeportivo/model/Deporte.java
@@ -1,4 +1,50 @@
 package ClubDeportivo.model;
 
 public class Deporte {
+    private String nombreDeporte;
+    private String descripcion;
+    private boolean seJuegaConPelota;
+    private double largo;
+    private double ancho;
+
+    public Deporte(String nombreDeporte,String descripcion,boolean seJuegaConPelota,double largo,double ancho) {
+        this.nombreDeporte = nombreDeporte;
+        this.descripcion = descripcion;
+        this.seJuegaConPelota = seJuegaConPelota;
+        this.largo = largo;
+        this.ancho = ancho;
+    }
+    //getters
+
+
+    public String getNombreDeporte() {
+        return this.nombreDeporte;
+    }
+
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    public boolean isSeJuegaConPelota() {
+        return this.seJuegaConPelota;
+    }
+
+    public double getLargo() {
+        return this.largo;
+    }
+
+    public double getAncho() {
+        return this.ancho;
+    }
+
+    @Override
+    public String toString() {
+        return "Deporte{" +
+                "Nombre del deporte:'" + nombreDeporte + '\'' +
+                ", Descripcion:'" + descripcion + '\'' +
+                ", Se juega con pelota?:" + seJuegaConPelota +
+                ", Dimension de la cancha:" + largo + ancho +
+                '}';
+    }
 }
+
--- src/ClubDeportivo/model/Jugador.java
+++ src/ClubDeportivo/model/Jugador.java
@@ -1,4 +1,8 @@
 package ClubDeportivo.model;
 
-public class Jugador {
+public class Jugador extends Persona  {
+
+    public Jugador(int dni, String nombre, String fechaDeNacimiento){
+        super(dni,nombre,fechaDeNacimiento);
+    }
 }
